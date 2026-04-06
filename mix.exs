389169c61e73b6460443b397c59a804431429234
Corey Powell <mistdragon100@gmail.com>
2026-04-06 12:59:36 -0500

    Raise minimum Elixir version from 1.11 to 1.17

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Freddy.Mixfile do
     [
       app: :freddy,
       version: "0.17.1",
-      elixir: "~> 1.11",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
